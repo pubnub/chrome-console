b933492314f26073a758ddbd1b64d189c6a7a3a0
Ian Jennings <ian@meetjennings.com>
2014-02-27 13:41:36 -0500

    get all nessesary info for logging

--- Panel/PreprocessorPanel.js
+++ Panel/PreprocessorPanel.js
@@ -48,7 +48,42 @@ function listen() {
   reloadButton.addEventListener('click', demoPreprocessor);
 
   chrome.devtools.network.onRequestFinished.addListener(function(request) {
-    console.log(request.request.url)
+
+    var parser = document.createElement('a'),
+      params = null,
+      channel = null;
+
+    parser.href = request.request.url;
+
+    if(parser.hostname.split('.')[1] == "pubnub") {
+
+      params = parser.pathname.split('/');
+
+      console.log('here!')
+      console.log(request)
+      console.log(params)
+
+      if(params[1] == "publish") {
+
+        channel = params[5];
+        console.log('publish to channel ' + channel + ' and message:');
+        console.log(JSON.parse(decodeURIComponent(params[7])));
+
+      }
+
+      if(params[1] == "subscribe") {
+
+        request.getContent(function(body){
+
+          channel = params[3];
+          console.log('subscribe to channel ' + channel + ' and message:');
+            console.log(JSON.parse(body)[0][0]);
+          });
+
+      }
+
+    }
+
   });
 
 }
@@ -69,7 +104,4 @@ function updateUI(preprocessedFiles) {
   });
 }
 
-
 })();
-
-
